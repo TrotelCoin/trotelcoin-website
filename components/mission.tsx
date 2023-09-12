8d913e2448ed521e277139b9b53aae19664c8537
atrtdeee <trotelalexandre@proton.me>
2023-09-12 15:50:49 +0200

    centered

--- components/mission.tsx
+++ components/mission.tsx
@@ -1,12 +1,6 @@
-const stats = [
-  { label: "courses available", value: "0" },
-  { label: "holders of $TROTEL", value: "40+" },
-  { label: "quizzes answered", value: "0" },
-];
-
 export default function Mission() {
   return (
-    <div className="mx-auto -mt-12 max-w-7xl px-6 sm:mt-0 lg:px-8 xl:-mt-8">
+    <div className="mx-auto justify-center flex text-center -mt-12 max-w-7xl px-6 sm:mt-0 lg:px-8 xl:-mt-8">
       <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           The mission.
@@ -17,7 +11,7 @@ export default function Mission() {
               The mission is to empower individuals with the knowledge and tools
               to navigate and thrive in the world of Web3.
             </p>
-            <div className="mt-10 max-w-xl text-base leading-7 text-gray-700">
+            <div className="mt-10 m-auto max-w-xl text-base leading-7 text-gray-700">
               <p>
                 We provide a comprehensive educational platform that simplifies
                 complex concepts and offers practical examples for learning
@@ -26,20 +20,6 @@ export default function Mission() {
                 drive widespread adoption of Web3 technologies.
               </p>
             </div>
-          </div>
-          <div className="lg:flex lg:flex-auto lg:justify-center">
-            <dl className="w-64 space-y-8 xl:w-80">
-              {stats.map((stat) => (
-                <div key={stat.label} className="flex flex-col-reverse gap-y-4">
-                  <dt className="text-base leading-7 text-gray-600">
-                    {stat.label}
-                  </dt>
-                  <dd className="text-5xl font-semibold tracking-tight text-gray-900">
-                    {stat.value}
-                  </dd>
-                </div>
-              ))}
-            </dl>
           </div>
         </div>
       </div>
