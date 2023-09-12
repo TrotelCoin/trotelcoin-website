18c2f0d65df7e3217061e10a704224a2be9f3207
atrtdeee <trotelalexandre@proton.me>
2023-09-12 09:05:48 +0200

    adding dark mode and header

---
+++ app/about.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Image from "next/image";
+import Header from "../components/header";
+
+export default function About() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
---
+++ components/header.tsx
@@ -0,0 +1,116 @@
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import "../app/globals.css";
+
+const navigation = [
+  { name: "Home", href: "#" },
+  {
+    name: "Whitepaper",
+    href: "https://github.com/TrotelCoin/trotelcoin-wiki/wiki",
+    target: "_blank",
+  },
+  { name: "Get started", href: "./getStarted" },
+  { name: "About", href: "./About" },
+];
+
+export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="bg-white dark:bg-slate-800">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <span className="sr-only">TrotelCoin</span>
+          <img
+            className="h-12 w-auto dark:invisible"
+            src="trotelcoin.svg"
+            alt="TrotelCoin logo"
+          />
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              target={item.target}
+              className="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-100"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+        <div className="flex flex-1 items-center justify-end gap-x-6">
+          <a
+            href="https://app.trotelcoin.com"
+            className="rounded-md bg-gray-900 px-3 py-2 text-sm dark:bg-gray-100 dark:text-gray-900 font-semibold text-gray-100 shadow-sm duration-200 hover:scale-110 hover:duration-200 hover:text-gray-100 dark:hover:text-gray-900 hover:no-underline focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+          >
+            Launch App
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6 text-gray-100" aria-hidden="true" />
+          </button>
+        </div>
+      </nav>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-slate-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center gap-x-6">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">TrotelCoin</span>
+              <img
+                className="h-12 w-auto dark:invisible"
+                src="trotelcoin.svg"
+                alt="TrotelCoin logo"
+              />
+            </a>
+            <a
+              href="https://app.trotelcoin.com"
+              className="ml-auto rounded-md bg-black px-3 py-2 text-sm font-semibold dark:bg-gray-100 dark:text-gray-900 text-white duration-200 shadow-sm hover:scale-110 hover:duration-200 hover:no-underline focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Launch App
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-100"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 dark:text-gray-100 text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  );
+}
