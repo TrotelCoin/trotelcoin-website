6d61734f131244d7c19ce75150b79cd24d1d10bd
atrtdeee <trotelalexandre@proton.me>
2023-09-12 09:09:37 +0200

    adding skeleton of hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Image from "next/image";
 import Header from "../components/header";
+import Hero from "../components/hero";
 
 export default function Home() {
   return (
     <div>
       <Header></Header>
+      <Hero></Hero>
     </div>
   );
 }
