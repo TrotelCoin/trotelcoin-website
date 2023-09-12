ef22d4babb272e084edcc98d660133e3036d7595
atrtdeee <trotelalexandre@proton.me>
2023-09-12 21:08:00 +0200

    minor fixes

--- components/mission.tsx
+++ components/mission.tsx
@@ -1,24 +1,26 @@
 export default function Mission() {
   return (
-    <div className="mx-auto justify-center flex text-center -mt-12 max-w-7xl px-6 sm:mt-0 lg:px-8 xl:-mt-8">
-      <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          The mission.
-        </h2>
-        <div className="mt-6 flex flex-col gap-x-8 gap-y-20 lg:flex-row">
-          <div className="lg:w-full lg:max-w-2xl lg:flex-auto">
-            <p className="text-xl leading-8 text-gray-600">
-              The mission is to empower individuals with the knowledge and tools
-              to navigate and thrive in the world of Web3.
-            </p>
-            <div className="mt-10 m-auto max-w-xl text-base leading-7 text-gray-700">
-              <p>
-                We provide a comprehensive educational platform that simplifies
-                complex concepts and offers practical examples for learning
-                about transactions, DeFi, and more. Our goal is to democratize
-                access to decentralized finance, foster a vibrant community, and
-                drive widespread adoption of Web3 technologies.
+    <div className="bg-white">
+      <div className="mx-auto justify-center flex text-center -mt-12 max-w-7xl px-6 sm:mt-0 lg:px-8 xl:-mt-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            The mission.
+          </h2>
+          <div className="mt-6 flex flex-col gap-x-8 gap-y-20 lg:flex-row">
+            <div className="lg:w-full lg:max-w-2xl lg:flex-auto">
+              <p className="text-xl leading-8 text-gray-600">
+                The mission is to empower individuals with the knowledge and
+                tools to navigate and thrive in the world of Web3.
               </p>
+              <div className="mt-10 m-auto max-w-xl text-base leading-7 text-gray-700">
+                <p>
+                  We provide a comprehensive educational platform that
+                  simplifies complex concepts and offers practical examples for
+                  learning about transactions, DeFi, and more. Our goal is to
+                  democratize access to decentralized finance, foster a vibrant
+                  community, and drive widespread adoption of Web3 technologies.
+                </p>
+              </div>
             </div>
           </div>
         </div>
