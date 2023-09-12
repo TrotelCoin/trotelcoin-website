ed0194bcd69669501a2cf1256008e49cb2b858c1
atrtdeee <trotelalexandre@proton.me>
2023-09-12 23:38:04 +0200

    minor fixes

--- components/getStartedHero.tsx
+++ components/getStartedHero.tsx
@@ -178,7 +178,7 @@ export default function GetStartedHero() {
     <div className="bg-white">
       <header className="bg-gray-900">
         <nav
-          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+          className="flex items-center justify-between p-6 lg:px-8"
           aria-label="Global"
         >
           <div className="flex lg:flex-1">
