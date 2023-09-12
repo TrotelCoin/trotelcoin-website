4b6c9f67c18f70ccab93f530132e420a6321f1d9
atrtdeee <trotelalexandre@proton.me>
2023-09-12 22:34:06 +0200

    responsive for ellipse

--- components/getStartedHero.tsx
+++ components/getStartedHero.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
-import { Dialog, RadioGroup } from "@headlessui/react";
+import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import {
   CheckIcon,
   XMarkIcon as XMarkIconMini,
+  ArrowPathIcon,
+  CloudArrowUpIcon,
+  LockClosedIcon,
 } from "@heroicons/react/20/solid";
 import Link from "next/link";
 
@@ -17,6 +20,157 @@ const navigation = [
   { name: "Get started", href: "/getStarted" },
   { name: "About", href: "/about" },
 ];
+
+const features = [
+  {
+    name: "Push to deploy",
+    description:
+      "Commodo nec sagittis tortor mauris sed. Turpis tortor quis scelerisque diam id accumsan nullam tempus. Pulvinar etiam lacus volutpat eu. Phasellus praesent ligula sit faucibus.",
+    href: "#",
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: "SSL certificates",
+    description:
+      "Pellentesque enim a commodo malesuada turpis eleifend risus. Facilisis donec placerat sapien consequat tempor fermentum nibh.",
+    href: "#",
+    icon: LockClosedIcon,
+  },
+  {
+    name: "Simple queues",
+    description:
+      "Pellentesque sit elit congue ante nec amet. Dolor aenean curabitur viverra suspendisse iaculis eget. Nec mollis placerat ultricies euismod ut condimentum.",
+    href: "#",
+    icon: ArrowPathIcon,
+  },
+];
+
+const pricing = {
+  tiers: [
+    {
+      name: "Explorer",
+      id: "tier-explorer",
+      href: "#",
+      featured: false,
+      description: "No Token Holding Requirement.",
+      price: "0",
+      mainFeatures: [
+        "Free access to all educational courses and resources",
+        "Participation in interactive quizzes and activities",
+      ],
+    },
+    {
+      name: "Voyager",
+      id: "tier-voyager",
+      href: "#",
+      featured: true,
+      description: "Holders of 5,000 TrotelCoin or more.",
+      price: "5000",
+      mainFeatures: [
+        "All benefits from the Pioneer and Explorer Tiers",
+        "Direct involvement in shaping the future of TrotelCoin through governance decisions",
+        "Exclusive access to advanced courses, workshops, and personalized mentoring sessions",
+        "Early access to experimental features and projects",
+        "Invitation to our annual crypto summit",
+      ],
+    },
+    {
+      name: "Pioneer",
+      id: "tier-pioneer",
+      href: "#",
+      featured: false,
+      description: "Holders of 1,000 TrotelCoin or more.",
+      price: "1000",
+      mainFeatures: [
+        "All benefits from the Explorer Tier",
+        "Early access to governance discussions and voting rights",
+        "Priority access to new educational resources",
+        "Exclusive updates on project developments",
+        "Access to additional in-depth content and tutorials",
+      ],
+    },
+  ],
+  sections: [
+    {
+      name: "Catered for business",
+      features: [
+        {
+          name: "Tax Savings",
+          tiers: { Starter: true, Scale: true, Growth: true },
+        },
+        {
+          name: "Easy to use accounting",
+          tiers: { Starter: true, Scale: true, Growth: true },
+        },
+        {
+          name: "Multi-accounts",
+          tiers: {
+            Starter: "3 accounts",
+            Scale: "Unlimited accounts",
+            Growth: "7 accounts",
+          },
+        },
+        {
+          name: "Invoicing",
+          tiers: {
+            Starter: "3 invoices",
+            Scale: "Unlimited invoices",
+            Growth: "10 invoices",
+          },
+        },
+        {
+          name: "Exclusive offers",
+          tiers: { Starter: false, Scale: true, Growth: true },
+        },
+        {
+          name: "6 months free advisor",
+          tiers: { Starter: false, Scale: true, Growth: true },
+        },
+        {
+          name: "Mobile and web access",
+          tiers: { Starter: false, Scale: true, Growth: false },
+        },
+      ],
+    },
+    {
+      name: "Other perks",
+      features: [
+        {
+          name: "24/7 customer support",
+          tiers: { Starter: true, Scale: true, Growth: true },
+        },
+        {
+          name: "Instant notifications",
+          tiers: { Starter: true, Scale: true, Growth: true },
+        },
+        {
+          name: "Budgeting tools",
+          tiers: { Starter: true, Scale: true, Growth: true },
+        },
+        {
+          name: "Digital receipts",
+          tiers: { Starter: true, Scale: true, Growth: true },
+        },
+        {
+          name: "Pots to separate money",
+          tiers: { Starter: false, Scale: true, Growth: true },
+        },
+        {
+          name: "Free bank transfers",
+          tiers: { Starter: false, Scale: true, Growth: false },
+        },
+        {
+          name: "Business debit card",
+          tiers: { Starter: false, Scale: true, Growth: false },
+        },
+      ],
+    },
+  ],
+};
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function GetStartedHero() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -114,6 +268,183 @@ export default function GetStartedHero() {
           </Dialog.Panel>
         </Dialog>
       </header>
+
+      <main>
+        <div className="isolate overflow-hidden">
+          <div className="flow-root bg-gray-900 py-16 sm:pt-32 lg:pb-0">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="relative z-10">
+                <h1 className="mx-auto max-w-4xl text-center text-5xl font-bold tracking-tight text-white">
+                  HODL TrotelCoin. Get Benefits.
+                </h1>
+                <p className="mx-auto mt-4 max-w-2xl text-center text-lg leading-8 text-white/60">
+                  Keep your TrotelCoin investment for potential benefits and
+                  rewards.
+                </p>
+                <div className="mt-16 flex justify-center">
+                  <div className="grid grid-cols-2 gap-x-1 rounded-full bg-white/5 p-1 text-center text-xs font-semibold leading-5 text-white"></div>
+                </div>
+              </div>
+              <div className="relative mx-auto mt-10 grid max-w-md grid-cols-1 gap-y-8 lg:mx-0 lg:-mb-14 lg:max-w-none lg:grid-cols-3">
+                <svg
+                  viewBox="0 0 1208 1024"
+                  aria-hidden="true"
+                  className="absolute -bottom-48 left-1/2 h-[64rem] -translate-x-1/2 translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] lg:-top-48 lg:bottom-auto lg:translate-y-0"
+                >
+                  <ellipse
+                    className="invisible lg:visible xl:visible 2xl:visible"
+                    cx={604}
+                    cy={512}
+                    fill="url(#d25c25d4-6d43-4bf9-b9ac-1842a30a4867)"
+                    rx={604}
+                    ry={512}
+                  />
+                  <defs>
+                    <radialGradient id="d25c25d4-6d43-4bf9-b9ac-1842a30a4867">
+                      <stop stopColor="#74b9ff" />
+                      <stop offset={1} stopColor="#0984e3" />
+                    </radialGradient>
+                  </defs>
+                </svg>
+                <div
+                  className="hidden lg:absolute lg:inset-x-px lg:bottom-0 lg:top-4 lg:block lg:rounded-t-2xl lg:bg-gray-800/80 lg:ring-1 lg:ring-white/10"
+                  aria-hidden="true"
+                />
+                {pricing.tiers.map((tier) => (
+                  <div
+                    key={tier.id}
+                    className={classNames(
+                      tier.featured
+                        ? "z-10 bg-white shadow-xl ring-1 ring-gray-900/10"
+                        : "bg-gray-800/80 ring-1 ring-white/10 lg:bg-transparent lg:pb-14 lg:ring-0",
+                      "relative rounded-2xl"
+                    )}
+                  >
+                    <div className="p-8 lg:pt-12 xl:p-10 xl:pt-14">
+                      <h2
+                        id={tier.id}
+                        className={classNames(
+                          tier.featured ? "text-gray-900" : "text-white",
+                          "text-sm font-semibold leading-6"
+                        )}
+                      >
+                        {tier.name}
+                      </h2>
+                      <div className="flex flex-col gap-6 sm:flex-row sm:items-end sm:justify-between lg:flex-col lg:items-stretch">
+                        <div className="mt-2 flex items-center gap-x-4">
+                          <p
+                            className={classNames(
+                              tier.featured ? "text-gray-900" : "text-white",
+                              "text-4xl font-bold tracking-tight"
+                            )}
+                          >
+                            {tier.price}
+                          </p>
+                          <div className="text leading-5">
+                            <p
+                              className={
+                                tier.featured ? "text-gray-900" : "text-white"
+                              }
+                            >
+                              $TROTEL
+                            </p>
+                          </div>
+                        </div>
+                        <a
+                          href="https://pancakeswap.finance/swap?outputCurrency=0xf04ab1a43cBA1474160B7B8409387853D7Be02d5"
+                          target="_blank"
+                          aria-describedby={tier.id}
+                          className={classNames(
+                            tier.featured
+                              ? "bg-blue-600 shadow-sm hover:bg-blue-500 hover:no-underline focus-visible:outline-blue-600"
+                              : "bg-white/10 hover:bg-white/20 hover:no-underline focus-visible:outline-white",
+                            "rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                          )}
+                        >
+                          Buy TrotelCoin
+                        </a>
+                      </div>
+                      <div className="mt-8 flow-root sm:mt-10">
+                        <ul
+                          role="list"
+                          className={classNames(
+                            tier.featured
+                              ? "divide-gray-900/5 border-gray-900/5 text-gray-600"
+                              : "divide-white/5 border-white/5 text-white",
+                            "-my-2 divide-y border-t text-sm leading-6 lg:border-t-0"
+                          )}
+                        >
+                          {tier.mainFeatures.map((mainFeature) => (
+                            <li key={mainFeature} className="flex gap-x-3 py-2">
+                              <CheckIcon
+                                className={classNames(
+                                  tier.featured
+                                    ? "text-blue-600"
+                                    : "text-gray-500",
+                                  "h-6 w-5 flex-none"
+                                )}
+                                aria-hidden="true"
+                              />
+                              {mainFeature}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="relative bg-gray-50 lg:pt-14">
+            <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+              <div className="bg-gray-50 py-24 sm:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                  <div className="mx-auto max-w-2xl lg:text-center">
+                    <h2 className="text-base font-semibold leading-7 text-indigo-600">
+                      Deploy faster
+                    </h2>
+                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                      Everything you need to deploy your app
+                    </p>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                      Quis tellus eget adipiscing convallis sit sit eget aliquet
+                      quis. Suspendisse eget egestas a elementum pulvinar et
+                      feugiat blandit at. In mi viverra elit nunc.
+                    </p>
+                  </div>
+                  <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+                    <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+                      {features.map((feature) => (
+                        <div key={feature.name} className="flex flex-col">
+                          <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                            <feature.icon
+                              className="h-5 w-5 flex-none text-indigo-600"
+                              aria-hidden="true"
+                            />
+                            {feature.name}
+                          </dt>
+                          <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                            <p className="flex-auto">{feature.description}</p>
+                            <p className="mt-6">
+                              <a
+                                href={feature.href}
+                                className="text-sm font-semibold leading-6 text-indigo-600"
+                              >
+                                Learn more <span aria-hidden="true">→</span>
+                              </a>
+                            </p>
+                          </dd>
+                        </div>
+                      ))}
+                    </dl>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
