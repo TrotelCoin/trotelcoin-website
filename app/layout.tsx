5af158e7968901cb8ea1857119c09be5f2a53327
atrtdeee <trotelalexandre@proton.me>
2023-09-12 17:55:16 +0200

    minor fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import Loading from "./loading";
 import { Suspense } from "react";
 import Hotjar from "@hotjar/browser";
+import Head from "next/head";
 
 const siteId = 3583733;
 const hotjarVersion = 6;
@@ -22,6 +23,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Suspense fallback={<Loading></Loading>}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>{children}</body>
       </Suspense>
     </html>
