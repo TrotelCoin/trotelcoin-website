a7c4e78fac4a27cbe6d42a3a64e5eabbb3f111fe
atrtdeee <trotelalexandre@proton.me>
2023-09-12 18:25:21 +0200

    minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,10 @@ import Roadmap from "@/components/roadmap";
 import Powered from "@/components/powered";
 import Footer from "@/components/footer";
 import Banner from "@/components/banner";
-import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Hero></Hero>
       <Token></Token>
       <Roadmap></Roadmap>
