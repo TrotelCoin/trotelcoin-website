2b5e4dd4a7037fcd9d3c1b762e12c7d4c221d294
atrtdeee <trotelalexandre@proton.me>
2023-09-13 10:17:01 +0200

    changing locales strategy

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 module.exports = {
   i18n: {
-    locales: ["en-US", "fr", "nl-NL"],
-    defaultLocale: "en-US",
-    domains: [
-      {
-        domain: "trotelcoin.com",
-        defaultLocale: "en-US",
-      },
-      {
-        domain: "trotelcoin.fr",
-        defaultLocale: "fr",
-      },
-    ],
+    locales: ['en-US', 'fr'],
+    defaultLocale: 'en-US',
   },
-};
+}
