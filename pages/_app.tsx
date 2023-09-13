06f175a2764fd36a827d31cb84b6de5cd546cbc1
atrtdeee <trotelalexandre@proton.me>
2023-09-13 10:21:21 +0200

    changing strategy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,8 @@
 import { AppProps } from "next/app";
 import { NextPage } from "next";
 import "../app/globals.css";
-import { useEffect } from "react";
-import { appWithTranslation } from "next-i18next";
-import i18n from "i18next";
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
-  useEffect(() => {
-    i18n.init({
-      lng: "fr",
-      resources: {
-        en: { translation: require("../locales/en.json") },
-        fr: { translation: require("../locales/fr.json") },
-      },
-      interpolation: { escapeValue: false },
-    });
-  }, []);
-
   return (
     <>
       <Component {...pageProps} />
