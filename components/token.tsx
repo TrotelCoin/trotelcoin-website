6aaafa7e53138be18e3d14d40cd8395576bfbd7d
atrtdeee <trotelalexandre@proton.me>
2023-09-13 15:31:46 +0200

    changing token price precision

--- components/token.tsx
+++ components/token.tsx
@@ -56,7 +56,7 @@ export default function Token() {
         <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
           <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-50 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start border-2 border-black">
             <p className="flex-none text-3xl font-bold tracking-tight text-gray-900">
-              {tokenPrice.toFixed(2)} $
+              {tokenPrice.toFixed(3)} $
             </p>
             <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
               <p className="text-lg font-semibold tracking-tight text-gray-900">
