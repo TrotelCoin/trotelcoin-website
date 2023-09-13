12bcf7affc6354512eb590aca80114e081e062db
atrtdeee <trotelalexandre@proton.me>
2023-09-13 10:15:45 +0200

    changing nextjs config for i18n

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  i18n: {
+    locales: ["en-US", "fr", "nl-NL"],
+    defaultLocale: "en-US",
+    domains: [
+      {
+        domain: "trotelcoin.com",
+        defaultLocale: "en-US",
+      },
+      {
+        domain: "trotelcoin.fr",
+        defaultLocale: "fr",
+      },
+    ],
+  },
+};
