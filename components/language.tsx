87919411280c35803a0902ce52e71e479b25d1e0
atrtdeee <trotelalexandre@proton.me>
2023-09-14 15:33:08 +0200

    minor fix

--- components/language.tsx
+++ components/language.tsx
@@ -1,17 +1,10 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { useLocale } from "@/context/LocaleContext";
 
 export default function Language() {
-  const { locale, setLocale } = useLocale() as {
-    locale: string;
-    setLocale: (locale: string) => void;
-  };
   const router = useRouter();
 
   const handleChangeLocale = (newLocale: string) => {
-    setLocale(newLocale);
-
     router.push(`/${newLocale}${router.pathname}`, undefined, {
       locale: newLocale,
     });
@@ -26,7 +19,7 @@ export default function Language() {
         id="language"
         name="language"
         className="mt-6 block w-auto rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-blue-600 sm:text-sm sm:leading-6"
-        defaultValue={locale}
+        defaultValue=""
         onChange={(e) => handleChangeLocale(e.target.value)}
       >
         <option value="">English</option>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,12 @@
 import { AppProps } from "next/app";
 import { NextPage } from "next";
 import "../app/globals.css";
-import { LocaleProvider } from "../context/LocaleContext";
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
-    <LocaleProvider>
+    <>
       <Component {...pageProps} />
-    </LocaleProvider>
+    </>
   );
 };
 
