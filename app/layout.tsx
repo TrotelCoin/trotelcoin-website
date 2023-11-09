9cc44110a42e42796689754340176d6390876ce6
trotelalexandre <trotelalexandre@proton.me>
2023-11-09 14:36:55 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { poppins } from "@/app/ui/fonts";
 import Header from "@/app/ui/header";
 import "@/app/globals.css";
 import Wagmi from "@/app/wagmi";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata = {
   title: "TrotelCoin | Learn & earn crypto",
@@ -55,6 +56,17 @@ export default function RootLayout({
         <body
           className={`bg-white dark:bg-black ${poppins.className} antialiased`}
         >
+          <NextTopLoader
+            color="#3b82f6"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            showSpinner={false}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #3b82f6,0 0 5px #3b82f6"
+          />
           <Header />
           {children}
         </body>
