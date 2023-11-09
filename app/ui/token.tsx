7369304304d318ea6ee22920c40c4c70d8e5853d
trotelalexandre <trotelalexandre@proton.me>
2023-11-09 14:21:45 +0100

    fix

--- app/ui/token.tsx
+++ app/ui/token.tsx
@@ -65,7 +65,7 @@ export default function Token() {
         <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
           <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl backdrop-blur-xl bg-gray-50 dark:bg-gray-900 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start border border-black/10 dark:border-white/10 hover:border-black/50 dark:hover:border-white/50">
             <p className="flex-none text-3xl font-bold tracking-tight text-black dark:text-white">
-              {tokenBalance == null || tokenBalance == undefined
+              {tokenPrice == null || tokenPrice == undefined
                 ? "0"
                 : tokenPrice.toFixed(3)}{" "}
               USD
@@ -81,7 +81,7 @@ export default function Token() {
           </div>
           <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl backdrop-blur-xl bg-gray-50 dark:bg-gray-900 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44 border border-black/10 dark:border-white/10 hover:border-black/50 dark:hover:border-white/50">
             <p className="flex-none text-3xl font-bold tracking-tight text-black dark:text-white">
-              {tokenBalance == null || tokenBalance == undefined
+              {tokenPrice == null || tokenPrice == undefined
                 ? "0"
                 : format((tokenPrice * 1e5).toFixed(0))}{" "}
               USD
