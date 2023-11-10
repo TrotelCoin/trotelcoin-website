ba44f62a3d1cb517928c289e3f461852008bf6a5
trotelalexandre <trotelalexandre@proton.me>
2023-11-10 07:56:54 +0100

    fix

--- app/ui/token.tsx
+++ app/ui/token.tsx
@@ -63,7 +63,11 @@ export default function Token() {
         </div>
         <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
           <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl backdrop-blur-xl bg-gray-50 dark:bg-gray-900 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start border border-black/10 dark:border-white/10 hover:border-black/50 dark:hover:border-white/50">
-            <p className="flex-none text-3xl font-bold tracking-tight text-black dark:text-white">
+            <p
+              className={`flex-none text-3xl font-bold tracking-tight text-black dark:text-white ${
+                error !== "" || tokenPrice === null ? "animate-pulse" : ""
+              }`}
+            >
               {error !== ""
                 ? "0"
                 : tokenPrice === null
@@ -81,7 +85,11 @@ export default function Token() {
             </div>
           </div>
           <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl backdrop-blur-xl bg-gray-50 dark:bg-gray-900 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44 border border-black/10 dark:border-white/10 hover:border-black/50 dark:hover:border-white/50">
-            <p className="flex-none text-3xl font-bold tracking-tight text-black dark:text-white">
+            <p
+              className={`flex-none text-3xl font-bold tracking-tight text-black dark:text-white ${
+                error !== "" || tokenPrice === null ? "animate-pulse" : ""
+              }`}
+            >
               {error !== ""
                 ? "0"
                 : tokenPrice === null
@@ -100,7 +108,13 @@ export default function Token() {
             </div>
           </div>
           <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl backdrop-blur-xl bg-gray-50 dark:bg-gray-900 p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28 border border-black/10 dark:border-white/10 hover:border-black/50 dark:hover:border-white/50">
-            <p className="flex-none text-3xl font-bold tracking-tight text-black dark:text-white">
+            <p
+              className={`flex-none text-3xl font-bold tracking-tight text-black dark:text-white ${
+                tokenBalance === null || tokenBalance === undefined
+                  ? "animate-pulse"
+                  : ""
+              }`}
+            >
               {tokenBalance === null || tokenBalance === undefined
                 ? "0"
                 : format(parseFloat(tokenBalance.formatted).toFixed(0))}{" "}
