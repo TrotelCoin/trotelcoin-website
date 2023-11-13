fcf692a494ad3166aeddfc787e1902acbf65f831
trotelalexandre <trotelalexandre@proton.me>
2023-11-13 21:23:54 +0100

    api total supply

---
+++ pages/api/totalSupply.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const apiKey = process.env.POLYGONSCAN_API_KEY;
+    const apiUrl = `https://api.polygonscan.com/api?module=stats&action=tokensupply&contractaddress=0x2275059f310E31C2F43B24A9932882196659e1C4&apikey=${apiKey}`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data.status === "1" && data.result) {
+      const numericalValue = parseFloat(data.result);
+      res.status(200).json({ numericalValue });
+    } else {
+      res.status(500).json({ error: "Invalid response from the API" });
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
